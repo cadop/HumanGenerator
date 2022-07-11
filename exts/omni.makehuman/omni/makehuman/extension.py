b91f77a45a701fa1cfb2db5ec4160530a2d4d309
Fruity-Grebbles <jhg29@njit.edu>
2022-07-11 18:06:50 -0400

    Add UI elements for parameter control + importing model into scene

--- exts/omni.makehuman/omni/makehuman/extension.py
+++ exts/omni.makehuman/omni/makehuman/extension.py
@@ -1,10 +1,15 @@
 import omni.ext
 import omni.ui as ui
 from . import mhcaller
+import omni
+
+# from . import assetconverter
 
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
 # instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
 # on_shutdown() is called.
+
+
 class MyExtension(omni.ext.IExt):
     # ext_id is current extension id. It can be used with extension manager to query additional information, like where
     # this extension is located on filesystem.
@@ -14,16 +19,38 @@ class MyExtension(omni.ext.IExt):
 
         # Create instance of manager class
         mh_call = mhcaller.MHCaller()
-        mh_call.filepath = 'D:/human.obj'
+        mh_call.filepath = "D:/human.obj"
 
-        self._window = ui.Window("My Window", width=300, height=300)
+        primpath = "/World/human"
+
+        self._window = ui.Window("Makehuman", width=300, height=300)
         with self._window.frame:
             with ui.VStack():
-                ui.Button("Save Human", clicked_fn=lambda: mh_call.store_obj())
-                with ui.HStack():
-                    field = ui.FloatField()
-                    field.model.add_value_changed_fn(lambda m:mh_call.set_age(m.get_value_as_int()))
+                with ui.CollapsableFrame("Phenotype"):
+                    with ui.HStack():
+                        ui.Label("Age", height=15, width=50)
+                        field = ui.FloatField(height=15, width=50)
+                        ui.FloatSlider(min=1, max=89, step=0.25, model=field.model)
+                        field.model.add_value_changed_fn(lambda m: mh_call.set_age(m.get_value_as_int()))
 
+                with ui.HStack():
+                    ui.Button(
+                        "add_to_scene",
+                        clicked_fn=lambda: self.add_to_scene(mh_call.filepath, primpath),
+                    )
+                    ui.Button("Save Human", clicked_fn=lambda: mh_call.store_obj())
 
     def on_shutdown(self):
-        print("[omni.makehuman] MyExtension shutdown")
+        print("[omni.makehuman] makehuman shutdown")
+
+    def add_to_scene(self, input_obj, primpath):
+
+        usd_context = omni.usd.get_context()
+
+        omni.kit.commands.execute(
+            "CreateReferenceCommand",
+            usd_context=usd_context,
+            path_to=primpath,
+            asset_path=input_obj,
+            instanceable=True,
+        )
