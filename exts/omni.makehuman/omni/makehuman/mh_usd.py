005a3b2fa29f88e361729f1c968065d9def34c3e
Fruity-Grebbles <jhg29@njit.edu>
2022-07-18 15:39:53 -0400

    Add each mesh to the scene separately

--- exts/omni.makehuman/omni/makehuman/mh_usd.py
+++ exts/omni.makehuman/omni/makehuman/mh_usd.py
@@ -3,6 +3,7 @@ import omni.usd
 import carb
 import numpy as np
 import io, os
+import re
 
 
 def add_to_scene(meshes):
@@ -12,69 +13,75 @@ def add_to_scene(meshes):
     # Filter out vertices we aren't meant to see
     meshes = [m.clone(filterMaskedVerts=True) for m in meshes]
 
-    mesh = meshes[0]
-    nPerFace = mesh.vertsPerFaceForExport
-    newvertindices = []
-    newuvindices = []
+    for mesh in meshes:
+        nPerFace = mesh.vertsPerFaceForExport
+        newvertindices = []
+        newuvindices = []
 
-    coords = mesh.getCoords()
-    for fn, fv in enumerate(mesh.fvert):
-        if not mesh.face_mask[fn]:
-            continue
-        # only include <nPerFace> verts for each face, and order them consecutively
-        newvertindices += [(fv[n]) for n in range(nPerFace)]
-        fuv = mesh.fuvs[fn]
-        # build an array of (u,v)s for each face
-        newuvindices += [(fuv[n]) for n in range(nPerFace)]
+        coords = mesh.getCoords()
+        for fn, fv in enumerate(mesh.fvert):
+            if not mesh.face_mask[fn]:
+                continue
+            # only include <nPerFace> verts for each face, and order them consecutively
+            newvertindices += [(fv[n]) for n in range(nPerFace)]
+            fuv = mesh.fuvs[fn]
+            # build an array of (u,v)s for each face
+            newuvindices += [(fuv[n]) for n in range(nPerFace)]
 
-    newvertindices = np.array(newvertindices)
+        newvertindices = np.array(newvertindices)
 
-    # Get stage.
-    stage = omni.usd.get_context().get_stage()
+        # Get stage.
+        stage = omni.usd.get_context().get_stage()
 
-    # Get default prim.
-    defaultPrim = stage.GetDefaultPrim()
+        # Get default prim.
+        defaultPrim = stage.GetDefaultPrim()
 
-    # Get root path.
-    rootPath = "/"
-    if defaultPrim.IsValid():
-        rootPath = defaultPrim.GetPath().pathString
-    carb.log_info(rootPath)
+        # Get root path.
+        rootPath = "/"
+        if defaultPrim.IsValid():
+            rootPath = defaultPrim.GetPath().pathString
+        carb.log_info(rootPath)
 
-    # Create mesh.
-    meshGeom = UsdGeom.Mesh.Define(stage, rootPath + "/mesh")
+        # Create mesh.
+        name = sanitize(mesh.name)
+        meshGeom = UsdGeom.Mesh.Define(stage, rootPath + "/" + name)
 
-    # Set vertices.
-    meshGeom.CreatePointsAttr(coords)
-    # meshGeom.CreatePointsAttr([(-10, 0, -10), (-10, 0, 10), (10, 0, 10), (10, 0, -10)])
+        # Set vertices.
+        meshGeom.CreatePointsAttr(coords)
+        # meshGeom.CreatePointsAttr([(-10, 0, -10), (-10, 0, 10), (10, 0, 10), (10, 0, -10)])
 
-    # Set normals.
-    meshGeom.CreateNormalsAttr(mesh.getNormals())
-    # meshGeom.CreateNormalsAttr([(0, 1, 0), (0, 1, 0), (0, 1, 0), (0, 1, 0)])
-    meshGeom.SetNormalsInterpolation("vertex")
+        # Set normals.
+        meshGeom.CreateNormalsAttr(mesh.getNormals())
+        # meshGeom.CreateNormalsAttr([(0, 1, 0), (0, 1, 0), (0, 1, 0), (0, 1, 0)])
+        meshGeom.SetNormalsInterpolation("vertex")
 
-    # Set face vertex count.
-    nface = [mesh.vertsPerFaceForExport] * len(mesh.nfaces)
-    meshGeom.CreateFaceVertexCountsAttr(nface)
-    # meshGeom.CreateFaceVertexCountsAttr([4])
+        # Set face vertex count.
+        nface = [mesh.vertsPerFaceForExport] * len(mesh.nfaces)
+        meshGeom.CreateFaceVertexCountsAttr(nface)
+        # meshGeom.CreateFaceVertexCountsAttr([4])
 
-    # Set face vertex indices.
-    meshGeom.CreateFaceVertexIndicesAttr(newvertindices)
-    # # meshGeom.CreateFaceVertexIndicesAttr([0, 1, 2, 3])
+        # Set face vertex indices.
+        meshGeom.CreateFaceVertexIndicesAttr(newvertindices)
+        # # meshGeom.CreateFaceVertexIndicesAttr([0, 1, 2, 3])
 
-    # # Set uvs.
-    texCoords = meshGeom.CreatePrimvar("st", Sdf.ValueTypeNames.TexCoord2fArray, UsdGeom.Tokens.faceVarying)
-    texCoords.Set(mesh.getUVs(newuvindices))
-    # texCoords.Set([(0, 1), (0, 0), (1, 0), (1, 1)])
+        # # Set uvs.
+        texCoords = meshGeom.CreatePrimvar("st", Sdf.ValueTypeNames.TexCoord2fArray, UsdGeom.Tokens.faceVarying)
+        texCoords.Set(mesh.getUVs(newuvindices))
+        # texCoords.Set([(0, 1), (0, 0), (1, 0), (1, 1)])
 
-    # # Subdivision is set to none.
-    meshGeom.CreateSubdivisionSchemeAttr().Set("none")
+        # # Subdivision is set to none.
+        meshGeom.CreateSubdivisionSchemeAttr().Set("none")
 
-    # # # Set position.
-    # UsdGeom.XformCommonAPI(meshGeom).SetTranslate((0.0, 0.0, 0.0))
+        # # # Set position.
+        # UsdGeom.XformCommonAPI(meshGeom).SetTranslate((0.0, 0.0, 0.0))
 
-    # # # Set rotation.
-    # UsdGeom.XformCommonAPI(meshGeom).SetRotate((0.0, 0.0, 0.0), UsdGeom.XformCommonAPI.RotationOrderXYZ)
+        # # # Set rotation.
+        # UsdGeom.XformCommonAPI(meshGeom).SetRotate((0.0, 0.0, 0.0), UsdGeom.XformCommonAPI.RotationOrderXYZ)
 
-    # # # Set scale.
-    # UsdGeom.XformCommonAPI(meshGeom).SetScale((1.0, 1.0, 1.0))
+        # # # Set scale.
+        # UsdGeom.XformCommonAPI(meshGeom).SetScale((1.0, 1.0, 1.0))
+
+
+def sanitize(s: str):
+    s = s.replace(".", "_")
+    return s
