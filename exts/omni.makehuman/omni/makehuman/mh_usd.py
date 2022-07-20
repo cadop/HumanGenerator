f9e044a9bd7635cb1c343545c290975a5acbb70e
Fruity-Grebbles <jhg29@njit.edu>
2022-07-20 17:17:42 -0400

    apply weights and prepare for skel import

--- exts/omni.makehuman/omni/makehuman/mh_usd.py
+++ exts/omni.makehuman/omni/makehuman/mh_usd.py
@@ -1,4 +1,4 @@
-from pxr import Usd, UsdGeom, UsdPhysics, UsdShade, Sdf, Gf, Tf
+from pxr import Usd, UsdGeom, UsdPhysics, UsdShade, Sdf, Gf, Tf, UsdSkel
 import omni.usd
 import carb
 import numpy as np
@@ -6,13 +6,72 @@ import io, os
 import re
 
 
-def add_to_scene(meshes):
+def add_to_scene(objects):
+
+    scale = 100
+    human = objects[0]
+
+    skel = human.getSkeleton()
+
+    meshes = [o.mesh for o in objects]
+
     if not isinstance(meshes, list):
         meshes = [meshes]
 
-    # Filter out vertices we aren't meant to see
-    meshes = [m.clone(filterMaskedVerts=True) for m in meshes]
+    # Filter out vertices we aren't meant to see and scale up the meshes
+    meshes = [m.clone(scale, filterMaskedVerts=True) for m in meshes]
 
+    # Scale our skeleton to match our human
+    if skel:
+        skel = skel.scaled(scale)
+
+    # Apply weights to the meshes (internal makehuman objects)
+    # Generate bone weights for all meshes up front so they can be reused for all
+    if skel:
+        rawWeights = human.getVertexWeights(human.getSkeleton())  # Basemesh weights
+        for mesh in meshes:
+            if mesh.object.proxy:
+                # Transfer weights to proxy
+                parentWeights = mesh.object.proxy.getVertexWeights(rawWeights, human.getSkeleton())
+            else:
+                parentWeights = rawWeights
+            # Transfer weights to face/vert masked and/or subdivided mesh
+            weights = mesh.getVertexWeights(parentWeights)
+
+            # Attach these vertexWeights to the mesh to pass them around the
+            # exporter easier, the cloned mesh is discarded afterwards, anyway
+            mesh.vertexWeights = weights
+    else:
+        # Attach trivial weights to the meshes
+        for mesh in meshes:
+            mesh.vertexWeights = None
+
+    # Get stage.
+    stage = omni.usd.get_context().get_stage()
+
+    # Get default prim.
+    defaultPrim = stage.GetDefaultPrim()
+
+    # Get root path.
+    rootPath = "/"
+    if defaultPrim.IsValid():
+        rootPath = defaultPrim.GetPath().pathString
+    carb.log_info(rootPath)
+
+    human_primpath = rootPath + "/human"
+
+    # Import skeleton to USD
+    if skel:
+        skelPrim = UsdSkel.Skeleton.Define(stage, human_primpath)
+        # add_joints(stage, human_primpath + "/", scene.rootnode)
+        # usd_skel = UsdSkel.Skeleton(skelPrim)
+        # joints_rel = usd_skel.GetJointsRel()
+        # for joint in joint_paths:
+        #     joints_rel.AppendTarget(joint)
+
+        # usd_skel.CreateRestTransformsAttr(rest_transforms)
+
+    # import meshes to USD
     for mesh in meshes:
         nPerFace = mesh.vertsPerFaceForExport
         newvertindices = []
@@ -29,18 +88,6 @@ def add_to_scene(meshes):
             newuvindices += [(fuv[n]) for n in range(nPerFace)]
 
         newvertindices = np.array(newvertindices)
-
-        # Get stage.
-        stage = omni.usd.get_context().get_stage()
-
-        # Get default prim.
-        defaultPrim = stage.GetDefaultPrim()
-
-        # Get root path.
-        rootPath = "/"
-        if defaultPrim.IsValid():
-            rootPath = defaultPrim.GetPath().pathString
-        carb.log_info(rootPath)
 
         # Create mesh.
         name = sanitize(mesh.name)
@@ -73,13 +120,13 @@ def add_to_scene(meshes):
         meshGeom.CreateSubdivisionSchemeAttr().Set("none")
 
         # # # Set position.
-        # UsdGeom.XformCommonAPI(meshGeom).SetTranslate((0.0, 0.0, 0.0))
+        UsdGeom.XformCommonAPI(meshGeom).SetTranslate((0.0, 0.0, 0.0))
 
         # # # Set rotation.
-        # UsdGeom.XformCommonAPI(meshGeom).SetRotate((0.0, 0.0, 0.0), UsdGeom.XformCommonAPI.RotationOrderXYZ)
+        UsdGeom.XformCommonAPI(meshGeom).SetRotate((0.0, 0.0, 0.0), UsdGeom.XformCommonAPI.RotationOrderXYZ)
 
         # # # Set scale.
-        # UsdGeom.XformCommonAPI(meshGeom).SetScale((1.0, 1.0, 1.0))
+        UsdGeom.XformCommonAPI(meshGeom).SetScale((1.0, 1.0, 1.0))
 
 
 def sanitize(s: str):
