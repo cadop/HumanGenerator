a69619ff48e6dbba744b4cdcdf9599d6b6876f07
Fruity-Grebbles <jhg29@njit.edu>
2022-07-20 17:17:01 -0400

    replace object meshes with objects to reference skels

--- exts/omni.makehuman/omni/makehuman/mhcaller.py
+++ exts/omni.makehuman/omni/makehuman/mhcaller.py
@@ -55,26 +55,10 @@ class MHCaller:
         # G.app.mhapi.exports.exportAsOBJ("D:/human.obj")
 
     @property
-    def meshes(self):
+    def objects(self):
         # Make sure proxies are up-to-date
         self.update()
-
-        objects = self.human.getObjects()
-        meshes = [o.mesh for o in objects]
-
-        # if config.hiddenGeom:
-        #     # Disable the face masking on copies of the input meshes
-        #     meshes = [m.clone(filterMaskedVerts=False) for m in meshes]
-        #     for m in meshes:
-        #         # Would be faster if we could tell clone() to do this, but it would
-        #         # make the interface more complex.
-        #         # We could also let the wavefront module do this, but this would
-        #         # introduce unwanted "magic" behaviour into the export function.
-        #         face_mask = np.ones(m.face_mask.shape, dtype=bool)
-        #         m.changeFaceMask(face_mask)
-        #         m.calcNormals()
-        #         m.updateIndexBuffer()
-        return meshes
+        return self.human.getObjects()
 
     def update(self):
         for obj in self.human.getObjects()[1:]:
