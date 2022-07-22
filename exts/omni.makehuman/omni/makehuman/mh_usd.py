492d3c3c5125afafbb95d41b4a15cfb2bbdf624c
Fruity-Grebbles <jhg29@njit.edu>
2022-07-22 13:01:23 -0400

    Add skeleton mesh bindings

--- exts/omni.makehuman/omni/makehuman/mh_usd.py
+++ exts/omni.makehuman/omni/makehuman/mh_usd.py
@@ -60,6 +60,7 @@ def add_to_scene(objects):
         rootPath = defaultPrim.GetPath().pathString
     carb.log_info(rootPath)
 
+    # usd_skel = None
     # Import skeleton to USD
     if skel:
         skel_root_path = rootPath + "/human"
@@ -86,6 +87,8 @@ def add_to_scene(objects):
         usd_skel.CreateBindTransformsAttr(skel_data["bind_transforms"])
         usd_skel.CreateRestTransformsAttr(skel_data["rest_transforms"])
 
+    usd_mesh_paths = []
+
     # import meshes to USD
     for mesh in meshes:
         nPerFace = mesh.vertsPerFaceForExport
@@ -106,7 +109,9 @@ def add_to_scene(objects):
 
         # Create mesh.
         name = sanitize(mesh.name)
-        meshGeom = UsdGeom.Mesh.Define(stage, rootPath + "/" + name)
+        usd_mesh_path = rootPath + "/" + name
+        usd_mesh_paths.append(usd_mesh_path)
+        meshGeom = UsdGeom.Mesh.Define(stage, usd_mesh_path)
 
         # Set vertices.
         meshGeom.CreatePointsAttr(coords)
@@ -143,6 +148,11 @@ def add_to_scene(objects):
         # # # # Set scale.
         # UsdGeom.XformCommonAPI(meshGeom).SetScale((1.0, 1.0, 1.0))
 
+    sdf_mesh_paths = [Sdf.Path(mesh_path) for mesh_path in usd_mesh_paths]
+
+    # root = skelRoot.GetPrim()
+    _create_mesh_bindings(sdf_mesh_paths, stage, usd_skel.GetPrim())
+
 
 def add_joints(path, node, skel_data):
 
@@ -156,7 +166,6 @@ def add_joints(path, node, skel_data):
 
     rxform = node.getRelativeMatrix()
     rxform = rxform.transpose()
-    carb.log_warn("")
     rest_transform = Gf.Matrix4d(rxform.tolist())
     s["rest_transforms"].append(rest_transform)
 
@@ -174,3 +183,111 @@ def sanitize(s: str):
     for c in illegal:
         s = s.replace(c, "_")
     return s
+
+
+# Modified from:
+# https://github.com/ColinKennedy/USD-Cookbook/tree/master/tools/export_usdskel_from_scratch
+# def _setup_meshes(meshes, skeleton):
+#     """Export `meshes` and then bind their USD Prims to a skeleton.
+
+#     Args:
+#         meshes (iter[str]):
+#             The paths to each Maya mesh that must be written to-disk.
+#             e.g. ["|some|pSphere1|pSphereShape1"].
+#         skeleton (`pxr.UsdSkel.Skeleton`):
+#             The USD Skeleton that the exported meshes will be paired with.
+
+#     Raises:
+#         RuntimeError: If `skeleton` has no ancestor UsdSkelRoot Prim.
+
+#     Returns:
+#         list[tuple[str, `pxr.UsdSkel.BindingAPI`]]:
+#             The Maya mesh which represents some USD mesh and the binding
+#             schema that is used to bind that mesh to the skeleton. We
+#             return these two values as pairs so that they don't have any
+#             chance of getting mixed up when other functions use them.
+
+#     """
+
+
+def _get_default_prim_path(path):
+    """`pxr.Sdf.Path`: Get the path of the defaultPrim of some USD file."""
+    stage = Usd.Stage.Open(path)
+    prim = stage.GetDefaultPrim()
+
+    return prim.GetPath()
+
+
+def _add_mesh_group_reference(stage, path, root):
+    """Add the meshes in some USD `path` file onto the given `stage` starting at `root`.
+
+    Important:
+        `default_added_to_root` must exist on stage before this function is called.
+
+    """
+    # Example:
+    #     default = Sdf.Path("/group1")
+    #     root = Sdf.Path("/Something/SkeletonRoot")
+    #     default_added_to_root = Sdf.Path("/Something/SkeletonRoot/group1")
+    #
+    default = _get_default_prim_path(path)
+    default_added_to_root = root.GetPath().AppendChild(str(default.MakeRelativePath("/")))
+    stage.GetPrimAtPath(default_added_to_root).GetReferences().AddReference(
+        os.path.relpath(path, os.path.dirname(stage.GetRootLayer().identifier))
+    )
+
+
+def _create_mesh_bindings(paths, stage, skeleton):
+    bindings = []
+
+    comment = "The values in this attribute were sorted by elementSize."
+
+    for usd_path in paths:
+        # `usd_path` is referenced under the UsdSkelRoot so we need to add
+        # its name to the path
+        #
+        prim = stage.GetPrimAtPath(usd_path)
+        binding = UsdSkel.BindingAPI.Apply(prim)
+        binding.CreateSkeletonRel().SetTargets([skeleton.GetPath()])
+        bindings.append(binding)
+
+    return bindings
+
+
+def _get_expected_mesh_paths(meshes, new_root):
+    """Make each mesh path relative to the given `new_root` UsdSkelRoot Prim path."""
+    relative_paths = [Sdf.Path(helper.convert_maya_path_to_usd(mesh).lstrip("/")) for mesh in meshes]
+
+    return [path_.MakeAbsolutePath(new_root) for path_ in relative_paths]
+
+
+# Note: This is a hacky trick but works well. We include
+# `exportSkin="auto"` to make USD export export joint influences
+# with our meshes. It also will try to link skinning to the mesh but
+# we will override those connections later.
+#
+# Basically, we use `cmds.usdExport` just for the joint influence
+# information and blow away the rest of the data it outputs in
+# other, stronger layers.
+#
+# Reference: https://graphics.pixar.com/usd/docs/Maya-USD-Plugins.html
+#
+# cmds.usdExport(meshes, file=path, exportSkin="auto")
+
+# root = UsdSkel.Root.Find(skeleton)
+
+# if not root:
+#     raise RuntimeError('Skeleton "{skeleton}" has no UsdSkelRoot.'.format(skeleton=skeleton.GetPath()))
+
+# root = root.GetPrim()
+# stage = root.GetStage()
+# bindings = _create_mesh_bindings(_get_expected_mesh_paths(meshes, root.GetPath()))
+# # Note: We add the mesh into an explicit `over` Prim instead of
+# # adding it directly onto `root` as a reference. This is so that we
+# # can have better control over what kind of meshes are loaded. e.g.
+# # In the future, we can switch this part to use a variant set to
+# # load different mesh LODs, for example.
+# #
+# _add_mesh_group_reference(stage, path, root)
+
+# return list(zip(meshes, bindings))
