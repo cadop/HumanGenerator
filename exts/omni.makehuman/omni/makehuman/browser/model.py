e3c2670cf3485520e6284c314d3a6e35df7b15f0
Fruity-Grebbles <jhg29@njit.edu>
2022-08-17 13:59:45 -0400

    remove unused code, add reference to list

--- exts/omni.makehuman/omni/makehuman/browser/model.py
+++ exts/omni.makehuman/omni/makehuman/browser/model.py
@@ -29,8 +29,9 @@ class MHAssetBrowserModel(FolderBrowserModel):
     Represent asset browser model
     """
 
-    def __init__(self, mhcaller, *args, **kwargs):
+    def __init__(self, mhcaller, list_widget, *args, **kwargs):
         self.mhcaller = mhcaller
+        self.list_widget = list_widget
         super().__init__(
             *args,
             setting_folders="/exts/omni.makehuman.browser.asset/folders",
@@ -38,23 +39,6 @@ class MHAssetBrowserModel(FolderBrowserModel):
             hide_file_without_thumbnails=False,
             **kwargs,
         )
-
-    # def execute(self, item: DetailItem) -> None:
-    #     # Create a Reference of the Props in the stage
-    #     stage = omni.usd.get_context().get_stage()
-    #     if not stage:
-    #         return
-
-    #     name = item.name_model.as_string.split(".")[0]
-
-    #     prim_path = omni.usd.get_stage_next_free_path(stage, "/" + name, True)
-
-    #     omni.kit.commands.execute(
-    #         "CreateReferenceCommand",
-    #         path_to=prim_path,
-    #         asset_path=item.url,
-    #         usd_context=omni.usd.get_context(),
-    #     )
 
     # Overwrite parent function to add thumbnails
     def create_detail_item(
--- exts/omni.makehuman/omni/makehuman/browser/proxy_browser.py
+++ exts/omni.makehuman/omni/makehuman/browser/proxy_browser.py
@@ -11,14 +11,16 @@ class AssetBrowserFrame(ui.Frame):
     Represent a window to show Assets
     """
 
-    def __init__(self, mhcaller, **kwargs):
+    def __init__(self, mhcaller, list_widget, **kwargs):
         super().__init__(**kwargs)
         self.mh_call = mhcaller
+        self.list_widget = list_widget
         self.set_build_fn(self._build_widget)
 
     def _build_widget(self):
         self._browser_model = MHAssetBrowserModel(
             self.mh_call,
+            self.list_widget,
             filter_file_suffixes=["mhpxy", "mhskel"],
             timeout=carb.settings.get_settings().get(
                 "/exts/omni.makehuman.browser.asset/data/timeout"
