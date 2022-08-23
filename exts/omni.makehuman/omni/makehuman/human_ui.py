ece9063f0dc60c0472c3d247851af626987a2240
Fruity-Grebbles <jhg29@njit.edu>
2022-08-23 13:07:55 -0400

    Add comments

--- exts/omni.makehuman/omni/makehuman/human_ui.py
+++ exts/omni.makehuman/omni/makehuman/human_ui.py
@@ -4,17 +4,29 @@ from .ui_widgets import *
 from .styles import *
 from . import mh_usd
 
-# class HumanPanel(ui.Frame):
+# class HumanPanel(ui.Frame): TODO remove
+
+
 class HumanPanel:
+    # UI Widget that includes list of modifiers, applied assets, and function buttons
     def __init__(self, mhcaller, **kwargs):
+        # TODO remove **kwargs
+        # Reference to manager class for Makehuman
         self.mh_call = mhcaller
 
         self._build_widget()
 
     def _build_widget(self):
+
+        # Human object
         human = self.mh_call.human
+
         with ui.HStack():
+
+            # UI for modifiers and parameters (affects physical characteristics)
             self.params = ParamPanel(human, width=300)
+
+            # UI for tracking applied assets and executing functions (eg. Create New Human)
             self.buttons = ButtonPanel(self.mh_call, width=200)
 
     def destroy(self):
@@ -25,13 +37,22 @@ class HumanPanel:
 
 class ParamPanel(ui.Frame):
     def __init__(self, human, **kwargs):
+        # Subclassing ui.Frame allows us to use styling on the whole widget
         super().__init__(**kwargs)
+
         self.human = human
+
+        # Reference to models for each modifier/parameter. The models store modifier
+        # data for reference in the UI
         self.models = []
+
         self.set_build_fn(self._build_widget)
 
     def _build_widget(self):
+
         def modifier_param(m):
+            # TODO add docstring
+            # Guess a suitable title from the modifier name
             tlabel = m.name.split("-")
             if "|" in tlabel[len(tlabel) - 1]:
                 tlabel = tlabel[:-1]
@@ -41,9 +62,11 @@ class ParamPanel(ui.Frame):
                 label = tlabel
             label = " ".join([word.capitalize() for word in label])
 
-            # Guess a suitable image path from target name
+            # Guess a suitable image path from modifier name
             tlabel = m.name.replace("|", "-").split("-")
             image = modifier_image(("%s.png" % "-".join(tlabel)).lower())
+
+            # Store modifier info in dataclass for building UI elements
             return Param(
                 label,
                 m.updateValue,
@@ -54,11 +77,19 @@ class ParamPanel(ui.Frame):
             )
 
         def group_params(group):
-            params = [modifier_param(m) for m in self.human.getModifiersByGroup(group)]
+            # TODO add docstring
+            params = [modifier_param(m)
+                      for m in self.human.getModifiersByGroup(group)]
             return params
 
         def build_macro_frame():
+            # TODO rename to indicate private function
+            # Shorten human reference for convenience
             human = self.human
+
+            # Explicitly create parameters for panel of macros (general modifiers that
+            # affect a group of targets). Otherwise these look bad. Creates a nice
+            # panel to have open by default
             macro_params = (
                 Param("Gender", human.setGender),
                 Param("Age", human.setAge),
@@ -67,34 +98,54 @@ class ParamPanel(ui.Frame):
                 Param("Height", human.setHeight),
                 Param("Proportions", human.setBodyProportions),
             )
+            # Create a model for storing macro parameter data
             macro_model = SliderEntryPanelModel(macro_params)
+
+            # Separate set of race parameters to also be included in the Macros group
+            # TODO make race parameters automatically normalize in UI
             race_params = (
                 Param("African", human.setAfrican),
                 Param("Asian", human.setAsian),
                 Param("Caucasian", human.setCaucasian),
             )
+            # Create a model for storing race parameter data
             race_model = SliderEntryPanelModel(race_params)
 
             self.models.append(macro_model)
             self.models.append(race_model)
 
+            # Create category widget for macros
             with ui.CollapsableFrame("Macros", style=styles.frame_style, height=0):
                 with ui.VStack():
+                    # Create panels for macros and race
                     self.panels = (
                         SliderEntryPanel(macro_model, label="General"),
                         SliderEntryPanel(race_model, label="Race"),
                     )
 
+        # The scrollable list of modifiers
         with ui.ScrollingFrame():
             with ui.VStack():
+                # Add the macros frame first
                 build_macro_frame()
-                macrogroups = [g for g in self.human.modifierGroups if "macrodetails" in g]
+
+                # Create a set of all modifier groups that include macros
+                macrogroups = [
+                    g for g in self.human.modifierGroups if "macrodetails" in g]
                 macrogroups = set(macrogroups)
-                allgroups = set(self.human.modifierGroups).difference(macrogroups)
+
+                # Remove macro groups from list of modifier groups as we have already
+                # included them explicitly
+                allgroups = set(
+                    self.human.modifierGroups).difference(macrogroups)
+
                 for group in allgroups:
+                    # Create a collapseable frame for each modifier group
                     with ui.CollapsableFrame(group.capitalize(), style=styles.frame_style, collapsed=True):
+                        # Model to hold panel parameters
                         model = SliderEntryPanelModel(group_params(group))
                         self.models.append(model)
+                        # Create panel of slider entries for modifier group
                         SliderEntryPanel(model)
 
     def destroy(self):
@@ -102,22 +153,31 @@ class ParamPanel(ui.Frame):
         for model in self.models:
             model.destroy()
 
-    # class ButtonPanel(ui.Frame):
+    # class ButtonPanel(ui.Frame): TODO remove
 
 
 class ButtonPanel:
     def __init__(self, mhcaller, **kwargs):
+        # Include instance of Makehuman wrapper class
         self.mh_call = mhcaller
+
+        # Pass **kwargs to buildwidget so we can apply styling as though ButtonPanel
+        # extended a base ui class
         self._build_widget(**kwargs)
 
     def _build_widget(self, **kwargs):
         with ui.VStack(**kwargs):
+            # Widget to list applied proxies TODO change to "Currently Applied Assets"
             self.drop = DropList("Currently Applied Proxies", self.mh_call)
+
+            # Updates current human in omniverse scene
             ui.Button(
                 "Update in Scene",
                 height=50,
                 clicked_fn=lambda: mh_usd.add_to_scene(self.mh_call),
             )
+
+            # Creates a new human in scene and resets modifiers and assets
             ui.Button(
                 "New Human",
                 height=50,
@@ -125,8 +185,15 @@ class ButtonPanel:
             )
 
     def new_human(self):
+
+        # Reset the human object in the makehuman wrapper. Also flags the human for
+        # name change to avoid overwriting existing humans
         self.mh_call.reset_human()
+
+        # Add the new, now reset human to the scene
         mh_usd.add_to_scene(self.mh_call)
 
     def destroy(self):
+        # No superclass to destroy, no models to destroy
+        # TODO do we need to reference the DropList model for destruction?
         pass
