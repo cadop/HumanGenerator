afd0121f6f184f7863a4df1e0300ae3cc13e7bb7
Fruity-Grebbles <jhg29@njit.edu>
2022-08-31 16:18:25 -0400

    Move download functionality to menu

--- exts/omni.makehuman/omni/makehuman/browser/options_menu.py
+++ exts/omni.makehuman/omni/makehuman/browser/options_menu.py
@@ -1,14 +1,16 @@
-from typing import Collection, Optional
+from typing import Optional, Callable
 from omni.kit.browser.core import OptionMenuDescription, OptionsMenu
 from omni.kit.browser.folder.core.models.folder_browser_item import FolderCollectionItem
+import omni.client, carb
 from .download import Downloader
-
+import aiohttp
+import os, zipfile
 class FolderOptionsMenu(OptionsMenu):
     """
     Represent options menu used in material browser. 
     """
 
-    def __init__(self, dl : Downloader):
+    def __init__(self, dl : Optional[Downloader]):
         super().__init__()
         self.dl = dl
 
@@ -25,13 +27,38 @@ class FolderOptionsMenu(OptionsMenu):
 
     def _get_menu_item_text(self) -> str:
         # Show download state if download starts
-        collection_item = self._browser_widget.collection_selection
-        if collection_item:
-            state = self._predownload_helper.get_download_state(collection_item.url)
-            if state is not None:
-                return "Download - " + state
-
         return "Download Assets"
 
-    def _on_download_assets(self) -> None:
-        pass
+    async def _on_download_assets(self, download_url : str, on_progress_fn: Callable[[float], None] = None) -> None:
+        ret_value = {"url": None}
+        async with aiohttp.ClientSession() as session:
+            content = bytearray()
+            # Download content from the given url
+            downloaded = 0
+            async with session.get(download_url) as response:
+                size = int(response.headers.get("content-length", 0))
+                if size > 0:
+                    async for chunk in response.content.iter_chunked(1024 * 512):
+                        content.extend(chunk)
+                        downloaded += len(chunk)
+                        if on_progress_fn:
+                            on_progress_fn(float(downloaded) / size)
+                else:
+                    if on_progress_fn:
+                        on_progress_fn(0)
+                    content = await response.read()
+                    if on_progress_fn:
+                        on_progress_fn(1)
+
+            if response.ok:
+                # Write to destination
+                filename = os.path.basename(download_url.split("?")[0])
+                dest_url = f"{dest_url}/{filename}"
+                (result, list_entry) = await omni.client.stat_async(dest_url)
+                ret_value["status"] = await omni.client.write_file_async(dest_url, content)
+                ret_value["url"] = dest_url
+            else:
+                carb.log_error(f"[access denied: {download_url}")
+                ret_value["status"] = omni.client.Result.ERROR_ACCESS_DENIED
+        z = zipfile.ZipFile(result)
+        return z.extractall(), ret_value
