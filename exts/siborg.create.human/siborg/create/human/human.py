6b33da4506573a218cf705ecdc0a92df0ac121b3
Fruity-Grebbles <jhg29@njit.edu>
2022-10-28 09:41:24 -0400

    Whitespace

--- exts/siborg.create.human/siborg/create/human/human.py
+++ exts/siborg.create.human/siborg/create/human/human.py
@@ -29,33 +29,33 @@ class Human:
         self.reset_human()
 
     def init_human(self):
-    """Initialize the human and set some required files from disk. This
-        includes the skeleton and any proxies (hair, clothes, accessories etc.)
-        The weights from the base skeleton must be transfered to the chosen
-        skeleton or else there will be unweighted verts on the meshes.
-        """
-    self.human = human.Human(files3d.loadMesh(
-        mh.getSysDataPath("3dobjs/base.obj"), maxFaces=5))
-    # set the makehuman instance human so that features (eg skeletons) can
-    # access it globally
-    self.G.app.selectedHuman = self.human
-    humanmodifier.loadModifiers(mh.getSysDataPath(
-        "modifiers/modeling_modifiers.json"), self.human)
-    # Add eyes
-    # self.add_proxy(data_path("eyes/high-poly/high-poly.mhpxy"), "eyes")
-    self.base_skel = skeleton.load(
-        mh.getSysDataPath("rigs/default.mhskel"),
-        self.human.meshData,
-    )
-    # cmu_skel = skeleton.load(data_path("rigs/cmu_mb.mhskel"), self.human.meshData)
-    # Build joint weights on our chosen skeleton, derived from the base
-    # skeleton
-    # cmu_skel.autoBuildWeightReferences(self.base_skel)
+        """Initialize the human and set some required files from disk. This
+            includes the skeleton and any proxies (hair, clothes, accessories etc.)
+            The weights from the base skeleton must be transfered to the chosen
+            skeleton or else there will be unweighted verts on the meshes.
+            """
+        self.human = human.Human(files3d.loadMesh(
+            mh.getSysDataPath("3dobjs/base.obj"), maxFaces=5))
+        # set the makehuman instance human so that features (eg skeletons) can
+        # access it globally
+        self.G.app.selectedHuman = self.human
+        humanmodifier.loadModifiers(mh.getSysDataPath(
+            "modifiers/modeling_modifiers.json"), self.human)
+        # Add eyes
+        # self.add_proxy(data_path("eyes/high-poly/high-poly.mhpxy"), "eyes")
+        self.base_skel = skeleton.load(
+            mh.getSysDataPath("rigs/default.mhskel"),
+            self.human.meshData,
+        )
+        # cmu_skel = skeleton.load(data_path("rigs/cmu_mb.mhskel"), self.human.meshData)
+        # Build joint weights on our chosen skeleton, derived from the base
+        # skeleton
+        # cmu_skel.autoBuildWeightReferences(self.base_skel)
 
-    self.human.setBaseSkeleton(self.base_skel)
-    # Actually add the skeleton
-    # self.human.setSkeleton(self.base_skel)
-    self.human.applyAllTargets()
+        self.human.setBaseSkeleton(self.base_skel)
+        # Actually add the skeleton
+        # self.human.setSkeleton(self.base_skel)
+        self.human.applyAllTargets()
 
     def reset_human(self):
         """Resets the human object to its initial state. This involves setting the
