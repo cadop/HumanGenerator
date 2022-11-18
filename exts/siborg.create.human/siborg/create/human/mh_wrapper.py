cd9d23540fd141570757b552c69f9a01b48e848a
Fruity-Grebbles <jhg29@njit.edu>
2022-11-18 16:20:06 -0500

    Remove vestigial module

--- exts/siborg.create.human/siborg/create/human/mh_wrapper.py
+++
@@ -1,7 +0,0 @@
-import makehuman
-
-# Makehuman loads most modules by manipulating the system path, so we have to
-# run this before we can run the rest of our makehuman imports
-makehuman.set_sys_path()
-# skeleton (imported from MakeHuman via path) provides Bone and Skeleton classes
-import skeleton
