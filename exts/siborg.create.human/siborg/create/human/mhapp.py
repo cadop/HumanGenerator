7b8c61281797183d188ff88226ccbe6417e7a36e
Fruity-Grebbles <jhg29@njit.edu>
2022-11-18 12:17:41 -0500

    Add imports, init human, remove instance vars

--- exts/siborg.create.human/siborg/create/human/mhapp.py
+++ exts/siborg.create.human/siborg/create/human/mhapp.py
@@ -1,3 +1,19 @@
+import makehuman
+
+# Makehuman loads most modules by manipulating the system path, so we have to
+# run this before we can run the rest of our makehuman imports
+
+makehuman.set_sys_path()
+
+import human
+import files3d
+import mh
+from core import G
+from mhmain import MHApplication
+import humanmodifier, skeleton
+import proxy, gui3d, events3d, targets
+from getpath import findFile
+
 class MHApp(object):
     """A singleton wrapper around the Makehuman app. Lets us use Makehuman functions without
     launching the whole application. Also holds all data about the state of our Human
@@ -11,38 +27,59 @@ class MHApp(object):
     human : Human
         Makehuman Human object. Encapsulates all human data (parameters, available)
         modifiers, skeletons, meshes, assets, etc) and functions.
-    filepath : str
-        Path on disk to which to write an OBJ directly from the Makehuman app. Does
-        not include any changes made inside of Omniverse (outside of those made in
-        the Makehuman extension).
-    default_name : str
-        The default human name to reset back to. All subsequent human names append
-        the first available number to the default name, ie: human, human_01, human_02,
-        etc.
-    name : str
-        The name of the current human. Used to update the prims of a human in the
-        USD stage without making a new human
-    is_reset : bool
-        A flag to indicate the human has been reset. Used for creating a new human
-        in the scene with default values, or resetting the values of the active
-        human. Indicates that the new human needs a new name.
     """
+
+    def __new__(cls):
+        """Singleton pattern. Only one instance of MHApp can exist at a time."""
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(MHApp, cls).__new__(cls)
+        return cls.instance
+
     def __init__(self):
         """Constructs an instance of MHCaller. This involves setting up the
         needed components to use makehuman modules independent of the GUI.
         This includes app globals (G) and the human object."""
         self.G = G
         self.human = None
-        self.filepath = None
-        # default name
-        self.default_name = "human"
-        self.name = self.default_name
-        self.is_reset = False
         self._config_mhapp()
         self.init_human()
 
+    def _config_mhapp(self):
+        """Declare and initialize the makehuman app, and move along if we
+        encounter any errors (omniverse sometimes fails to purge the app
+        singleton on extension reload, which can throw an error. This just means
+        the app already exists)
+        """
+        try:
+            self.G.app = MHApplication()
+        except:
+            return
 
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-        cls.instance = super(MHApp, cls).__new__(cls)
-        return cls.instance
+    def init_human(self):
+        """Initialize the human and set some required files from disk. This
+        includes the skeleton and any proxies (hair, clothes, accessories etc.)
+        The weights from the base skeleton must be transfered to the chosen
+        skeleton or else there will be unweighted verts on the meshes.
+        """
+        self.human = human.Human(files3d.loadMesh(
+            mh.getSysDataPath("3dobjs/base.obj"), maxFaces=5))
+        # set the makehuman instance human so that features (eg skeletons) can
+        # access it globally
+        self.G.app.selectedHuman = self.human
+        humanmodifier.loadModifiers(mh.getSysDataPath(
+            "modifiers/modeling_modifiers.json"), self.human)
+        # Add eyes
+        # self.add_proxy(data_path("eyes/high-poly/high-poly.mhpxy"), "eyes")
+        self.base_skel = skeleton.load(
+            mh.getSysDataPath("rigs/default.mhskel"),
+            self.human.meshData,
+        )
+        # cmu_skel = skeleton.load(data_path("rigs/cmu_mb.mhskel"), self.human.meshData)
+        # Build joint weights on our chosen skeleton, derived from the base
+        # skeleton
+        # cmu_skel.autoBuildWeightReferences(self.base_skel)
+
+        self.human.setBaseSkeleton(self.base_skel)
+        # Actually add the skeleton
+        # self.human.setSkeleton(self.base_skel)
+        self.human.applyAllTargets()
