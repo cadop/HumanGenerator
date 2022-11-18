cd62f51fb489b0b62ccabcece8b98f8ecdd1c0a7
Fruity-Grebbles <jhg29@njit.edu>
2022-11-18 16:45:20 -0500

    add sanitize method for paths

--- exts/siborg.create.human/siborg/create/human/shared.py
+++ exts/siborg.create.human/siborg/create/human/shared.py
@@ -18,3 +18,27 @@ def data_path(path):
     # Uses an absolute path, and then works its way up the folder directory to find the data folder
     data = str(Path(__file__).parents[3]) + "/data/" + path
     return data
+
+
+def sanitize(s: str):
+    """Sanitize strings for use a prim names. Strips and replaces illegal
+    characters.
+
+    Parameters
+    ----------
+    s : str
+        Input string
+
+    Returns
+    -------
+    s : str
+        Primpath-safe output string
+    """
+    # List of illegal characters
+    # TODO create more comprehensive list
+    # TODO switch from blacklisting illegal characters to whitelisting valid ones
+    illegal = (".", "-")
+    for c in illegal:
+        # Replace illegal characters with underscores
+        s = s.replace(c, "_")
+    return s
