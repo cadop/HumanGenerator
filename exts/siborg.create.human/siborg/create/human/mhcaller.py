7d66ecc89e52fda24a93b6bfa8047e3b5257bcda
Fruity-Grebbles <jhg29@njit.edu>
2022-12-02 17:03:42 -0500

    instance  objects convention -> class objects

--- exts/siborg.create.human/siborg/create/human/mhcaller.py
+++ exts/siborg.create.human/siborg/create/human/mhcaller.py
@@ -28,11 +28,11 @@ from .shared import data_path
 class classproperty:
     """Class property decorator. Allows us to define a property on a class
     method rather than an instance method."""
-    def __init__(self, fget):
-        self.fget = fget
+    def __init__(cls, fget):
+        cls.fget = fget
 
-    def __get__(self, obj, owner):
-        return self.fget(owner)
+    def __get__(cls, obj, owner):
+        return cls.fget(owner)
 
 
 class MHCaller:
@@ -53,12 +53,12 @@ class MHCaller:
     G = G
     human = None
 
-    def __init__(self):
+    def __init__(cls):
         """Constructs an instance of MHCaller. This involves setting up the
         needed components to use makehuman modules independent of the GUI.
         This includes app globals (G) and the human object."""
-        self._config_mhapp()
-        self.init_human()
+        cls._config_mhapp()
+        cls.init_human()
 
     def __new__(cls):
         """Singleton pattern. Only one instance of MHCaller can exist at a time."""
@@ -67,85 +67,85 @@ class MHCaller:
         return cls.instance
 
     @classmethod
-    def _config_mhapp(self):
+    def _config_mhapp(cls):
         """Declare and initialize the makehuman app, and move along if we
         encounter any errors (omniverse sometimes fails to purge the app
         singleton on extension reload, which can throw an error. This just means
         the app already exists)
         """
         try:
-            self.G.app = MHApplication()
+            cls.G.app = MHApplication()
         except:
             return
 
-        self.human_mapper = {}
+        cls.human_mapper = {}
 
     @classmethod
-    def reset_human(self):
+    def reset_human(cls):
         """Resets the human object to its initial state. This involves setting the
         human's name to its default, resetting all modifications, and resetting all
         proxies. Does not reset the skeleton. Also flags the human as having been
         reset so that the new name can be created when adding to the Usd stage.
         """
-        self.human.resetMeshValues()
+        cls.human.resetMeshValues()
         # Restore eyes
-        # self.add_proxy(data_path("eyes/high-poly/high-poly.mhpxy"), "eyes")
+        # cls.add_proxy(data_path("eyes/high-poly/high-poly.mhpxy"), "eyes")
         # Remove skeleton
-        self.human.skeleton = None
-        # HACK Set the age to itself to force an update of targets
-        self.human.setAge(self.human.getAge())
+        cls.human.skeleton = None
+        # HACK Set the age to itcls to force an update of targets
+        cls.human.setAge(cls.human.getAge())
 
     @classmethod
-    def init_human(self):
+    def init_human(cls):
         """Initialize the human and set some required files from disk. This
         includes the skeleton and any proxies (hair, clothes, accessories etc.)
         The weights from the base skeleton must be transfered to the chosen
         skeleton or else there will be unweighted verts on the meshes.
         """
-        self.human = human.Human(files3d.loadMesh(mh.getSysDataPath("3dobjs/base.obj"), maxFaces=5))
+        cls.human = human.Human(files3d.loadMesh(mh.getSysDataPath("3dobjs/base.obj"), maxFaces=5))
         # set the makehuman instance human so that features (eg skeletons) can
         # access it globally
-        self.G.app.selectedHuman = self.human
-        humanmodifier.loadModifiers(mh.getSysDataPath("modifiers/modeling_modifiers.json"), self.human)
+        cls.G.app.selectedHuman = cls.human
+        humanmodifier.loadModifiers(mh.getSysDataPath("modifiers/modeling_modifiers.json"), cls.human)
         # Add eyes
-        # self.add_proxy(data_path("eyes/high-poly/high-poly.mhpxy"), "eyes")
-        self.base_skel = skeleton.load(
+        # cls.add_proxy(data_path("eyes/high-poly/high-poly.mhpxy"), "eyes")
+        cls.base_skel = skeleton.load(
             mh.getSysDataPath("rigs/default.mhskel"),
-            self.human.meshData,
+            cls.human.meshData,
         )
-        # cmu_skel = skeleton.load(data_path("rigs/cmu_mb.mhskel"), self.human.meshData)
+        # cmu_skel = skeleton.load(data_path("rigs/cmu_mb.mhskel"), cls.human.meshData)
         # Build joint weights on our chosen skeleton, derived from the base
         # skeleton
-        # cmu_skel.autoBuildWeightReferences(self.base_skel)
+        # cmu_skel.autoBuildWeightReferences(cls.base_skel)
 
-        self.human.setBaseSkeleton(self.base_skel)
+        cls.human.setBaseSkeleton(cls.base_skel)
         # Actually add the skeleton
-        # self.human.setSkeleton(self.base_skel)
-        self.human.applyAllTargets()
+        # cls.human.setSkeleton(cls.base_skel)
+        cls.human.applyAllTargets()
 
     @classproperty
-    def objects(self):
+    def objects(cls):
         """List of objects attached to the human.
 
         Returns
         -------
         list of: guiCommon.Object
             All 3D objects included in the human. This includes the human
-            itself, as well as any proxies
+            itcls, as well as any proxies
         """
         # Make sure proxies are up-to-date
-        self.update()
-        return self.human.getObjects()
+        cls.update()
+        return cls.human.getObjects()
 
     @classproperty
-    def meshes(self):
+    def meshes(cls):
         """All of the meshes of all of the objects attached to a human. This
-        includes the mesh of the human itself as well as the meshes of all proxies
+        includes the mesh of the human itcls as well as the meshes of all proxies
         (clothing, hair, musculature, eyes, etc.)"""
-        return [o.mesh for o in self.objects]
+        return [o.mesh for o in cls.objects]
 
     @classproperty
-    def modifiers(self):
+    def modifiers(cls):
         """List of modifers attached to the human. These are all macros as well as any
         individual modifiers which have changed.
         Returns
@@ -153,25 +153,25 @@ class MHCaller:
         list of: humanmodifier.Modifier
             The macros and changed modifiers included in the human
         """
-        return [m for m in self.human.modifiers if m.getValue() or m.isMacro()]
+        return [m for m in cls.human.modifiers if m.getValue() or m.isMacro()]
 
     @classproperty
-    def proxies(self):
+    def proxies(cls):
         """List of proxies attached to the human.
         Returns
         -------
         list of: proxy.Proxy
             All proxies included in the human
         """
-        return self.human.getProxies()
+        return cls.human.getProxies()
 
     @classmethod
-    def update(self):
+    def update(cls):
         """Propagate changes to meshes and proxies"""
         # For every mesh object except for the human (first object), update the
         # mesh and corresponding proxy
         # See https://github.com/makehumancommunity/makehuman/search?q=adaptproxytohuman
-        for obj in self.human.getObjects()[1:]:
+        for obj in cls.human.getObjects()[1:]:
             mesh = obj.getSeedMesh()
             pxy = obj.getProxy()
             # Update the proxy
@@ -180,21 +180,21 @@ class MHCaller:
             mesh.update()
 
     # TODO remove this function. We can convert from USD if we want to export
-    def store_obj(self, filepath=None):
+    def store_obj(cls, filepath=None):
         """Write obj file to disk using makehuman's built-in exporter
 
         Parameters
         ----------
         filepath : str, optional
-            Path on disk to which to write the file. If none, uses self.filepath
+            Path on disk to which to write the file. If none, uses cls.filepath
         """
         if filepath is None:
-            filepath = self.filepath
+            filepath = cls.filepath
 
-        wavefront.writeObjFile(filepath, self.meshes)
+        wavefront.writeObjFile(filepath, cls.meshes)
 
     @classmethod
-    def add_proxy(self, proxypath : str, proxy_type  : str = None):
+    def add_proxy(cls, proxypath : str, proxy_type  : str = None):
         """Load a proxy (hair, nails, clothes, etc.) and apply it to the human
 
         Parameters
@@ -211,11 +211,11 @@ class MHCaller:
         #  See: http://www.makehumancommunity.org/forum/viewtopic.php?f=9&t=17182&sid=7c2e6843275d8c6c6e70288bc0a27ae9
         # Get proxy type if none is given
         if proxy_type is None:
-            proxy_type = self.guess_proxy_type(proxypath)
+            proxy_type = cls.guess_proxy_type(proxypath)
         # Load the proxy
-        pxy = proxy.loadProxy(self.human, proxypath, type=proxy_type)
+        pxy = proxy.loadProxy(cls.human, proxypath, type=proxy_type)
         # Get the mesh and Object3D object from the proxy applied to the human
-        mesh, obj = pxy.loadMeshAndObject(self.human)
+        mesh, obj = pxy.loadMeshAndObject(cls.human)
         # TODO is this next line needed?
         mesh.setPickable(True)
         # TODO Can this next line be deleted? The app isn't running
@@ -229,25 +229,25 @@ class MHCaller:
 
         # Set the object to be subdivided if the human is subdivided
         # TODO is this needed?
-        obj.setSubdivided(self.human.isSubdivided())
+        obj.setSubdivided(cls.human.isSubdivided())
 
 
         # Set/add proxy based on type
         if proxy_type == "eyes":
-            self.human.setEyesProxy(pxy)
+            cls.human.setEyesProxy(pxy)
         elif proxy_type == "clothes":
-            self.human.addClothesProxy(pxy)
+            cls.human.addClothesProxy(pxy)
         elif proxy_type == "eyebrows":
-            self.human.setEyebrowsProxy(pxy)
+            cls.human.setEyebrowsProxy(pxy)
         elif proxy_type == "eyelashes":
-            self.human.setEyelashesProxy(pxy)
+            cls.human.setEyelashesProxy(pxy)
         elif proxy_type == "hair":
-            self.human.setHairProxy(pxy)
+            cls.human.setHairProxy(pxy)
         else:
             # Body proxies (musculature, etc)
-            self.human.setProxy(pxy)
+            cls.human.setProxy(pxy)
 
-        vertsMask = np.ones(self.human.meshData.getVertexCount(), dtype=bool)
+        vertsMask = np.ones(cls.human.meshData.getVertexCount(), dtype=bool)
         proxyVertMask = proxy.transferVertexMaskToProxy(vertsMask, pxy)
         # Apply accumulated mask from previous layers on this proxy
         obj.changeVertexMask(proxyVertMask)
@@ -256,12 +256,12 @@ class MHCaller:
         # TODO add toggle for this feature in UI
         # verts = np.argwhere(pxy.deleteVerts)[..., 0]
         # vertsMask[verts] = False
-        # self.human.changeVertexMask(vertsMask)
+        # cls.human.changeVertexMask(vertsMask)
 
     Proxy = TypeVar("Proxy")
 
     @classmethod
-    def remove_proxy(self, proxy: Proxy):
+    def remove_proxy(cls, proxy: Proxy):
         """Removes a proxy from the human. Executes a particular method for removal
         based on proxy type.
 
@@ -273,23 +273,23 @@ class MHCaller:
         proxy_type = proxy.type.lower()
         # Use MakeHuman internal methods to remove proxy based on type
         if proxy_type == "eyes":
-            self.human.setEyesProxy(None)
+            cls.human.setEyesProxy(None)
         elif proxy_type == "clothes":
-            self.human.removeClothesProxy(proxy.uuid)
+            cls.human.removeClothesProxy(proxy.uuid)
         elif proxy_type == "eyebrows":
-            self.human.setEyebrowsProxy(None)
+            cls.human.setEyebrowsProxy(None)
         elif proxy_type == "eyelashes":
-            self.human.setEyelashesProxy(None)
+            cls.human.setEyelashesProxy(None)
         elif proxy_type == "hair":
-            self.human.setHairProxy(None)
+            cls.human.setHairProxy(None)
         else:
             # Body proxies (musculature, etc)
-            self.human.setProxy(None)
+            cls.human.setProxy(None)
 
     Skeleton = TypeVar("Skeleton")
 
     @classmethod
-    def remove_item(self, item : Union[Skeleton, Proxy]):
+    def remove_item(cls, item : Union[Skeleton, Proxy]):
         """Removes a Makehuman asset from the human. Assets include Skeletons
         as well as proxies. Determines removal method based on asset object type.
 
@@ -300,12 +300,12 @@ class MHCaller:
         """
         # TODO handle removing skeletons
         if isinstance(item, proxy.Proxy):
-            self.remove_proxy(item)
+            cls.remove_proxy(item)
         else:
             return
 
     @classmethod
-    def add_item(self, path : str):
+    def add_item(cls, path : str):
         """Add a Makehuman asset (skeleton or proxy) to the human.
 
         Parameters
@@ -314,12 +314,12 @@ class MHCaller:
             Path to the asset on disk
         """
         if "mhpxy" in path or "mhclo" in path:
-            self.add_proxy(path)
+            cls.add_proxy(path)
         elif "mhskel" in path:
-            self.set_skel(path)
+            cls.set_skel(path)
 
     @classmethod
-    def set_skel(self, path : str):
+    def set_skel(cls, path : str):
         """Change the skeleton applied to the human. Loads a skeleton from disk.
         The skeleton position can be used to drive the human position in the scene.
 
@@ -329,15 +329,15 @@ class MHCaller:
             The path to the skeleton to load from disk
         """
         # Load skeleton from path
-        skel = skeleton.load(path, self.human.meshData)
+        skel = skeleton.load(path, cls.human.meshData)
         # Build skeleton weights based on base skeleton
-        skel.autoBuildWeightReferences(self.base_skel)
+        skel.autoBuildWeightReferences(cls.base_skel)
         # Set the skeleton and update the human
-        self.human.setSkeleton(skel)
-        self.human.applyAllTargets()
+        cls.human.setSkeleton(skel)
+        cls.human.applyAllTargets()
 
     @classmethod
-    def guess_proxy_type(self, path : str):
+    def guess_proxy_type(cls, path : str):
         """Guesses a proxy's type based on the path from which it is loaded.
 
         Parameters
