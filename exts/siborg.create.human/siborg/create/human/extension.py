ca7ceda24a9875c2a3f347c91fe78d36f60c32b9
Joshua Grebler <jhg29@njit.edu>
2023-07-26 16:56:07 -0400

    Install packages one at a time

--- exts/siborg.create.human/siborg/create/human/extension.py
+++ exts/siborg.create.human/siborg/create/human/extension.py
@@ -11,11 +11,17 @@ from typing import Union
 import threading
 from .shared import Downloader, data_path
 
+# Make sure the required packages are installed for this extension
+# __requires__ = ['numpy>=1.17.4', 'PyQt5>=5.12.8','PyOpenGL>=3.1.0']
+# import pkg_resources
+
 # Omniverse ships with an API for installing python packages into the
 # internal python environment. This is usually done in extension.toml,
 # but we do it in code to prevent the extension from hanging while
-# installing the package.
-from omni.kit import pipapi
+# installing the package. We provide this asyncronously from the 
+# Downloader class so that we can update the progress bar while the
+# package is downloading and installing.
+
 
 from .window import MHWindow, WINDOW_TITLE, MENU_PATH
 
@@ -56,7 +62,9 @@ class MakeHumanExtension(omni.ext.IExt):
         except ModuleNotFoundError:
 
             # Start a thread to install makehuman
-            asyncio.ensure_future(self.install_makehuman(self.post_install))
+            install = threading.Thread(target=asyncio.run,
+                                       args=(self.install_makehuman(self.post_install),))
+            install.start()
         
 
         ui.Workspace.show_window(WINDOW_TITLE)
@@ -73,21 +81,41 @@ class MakeHumanExtension(omni.ext.IExt):
         """
 
         # Create a downloader instance. We'll add a progress bar when we build the UI
-        self.downloader = Downloader(self._window.progress_model.set_value)
+        downloader = Downloader(self._window.progress_model.set_value)
+
+        print("Checking for required packages...")
 
         try:
-            print("Attempting to install makehuman...")
-            result = await self.downloader.download(
-                "https://test-files.pythonhosted.org/packages/bc/fd/749c9a9eb29383850a4de5589767e0a37609b1cb71fbc0c41fb6b7f75e42/makehuman-1.2.2-py3-none-any.whl",
+            import numpy
+        except ModuleNotFoundError:
+            print("numpy not found. Installing...")
+            downloader.enqueue_install("numpy")
+        try:
+            import PyQt5
+        except ModuleNotFoundError:
+            print("PyQt5 not found. Installing...")
+            downloader.enqueue_install("PyQt5-sip")
+            downloader.enqueue_install("PyQt5-Qt5")
+            downloader.enqueue_install("PyQt5")
+        try:
+            import PyOpenGL
+        except ModuleNotFoundError:
+            print("PyOpenGL not found. Installing...")
+            downloader.enqueue_install("PyOpenGL")
+
+        print("Required packages installed. Installing makehuman...")
+        downloader.enqueue_download("https://test-files.pythonhosted.org/packages/bc/fd/749c9a9eb29383850a4de5589767e0a37609b1cb71fbc0c41fb6b7f75e42/makehuman-1.2.2-py3-none-any.whl",
                 data_path(""), unzip = False)
-            # Install makehuman
-            pipapi.install(result.get("url"),"makehuman", extra_args=["--no-compile"])
-            import makehuman
-            if callback:
-                callback()
-        except Exception as e:
-            print(type(e))
-            print(f"Error installing makehuman: {e}")
+        
+        # Wait for the download to complete
+        results = await downloader.run_tasks()
+        # Get the result of the download, the last item in the task list
+        result = results[-1]
+        # Install makehuman
+        await downloader.pip_install(result.get("url"),"makehuman", extra_args=["--no-compile"])
+        import makehuman
+        if callback:
+            callback()
 
     def post_install(self):
         # This function is called after makehuman is installed
