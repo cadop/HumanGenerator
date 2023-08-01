ccfa9c8bc8b16711eeb41c4556a6be2d4506dc0d
Joshua Grebler <jhg29@njit.edu>
2023-08-01 16:15:41 -0400

    remove makehuman-dependent UI components

--- exts/siborg.create.human/siborg/create/human/window.py
+++ exts/siborg.create.human/siborg/create/human/window.py
@@ -1,12 +1,11 @@
 from .ext_ui import ParamPanelModel, ParamPanel, NoSelectionNotification
-from .browser import MHAssetBrowserModel, AssetBrowserFrame
-from .human import Human
-from .mhcaller import MHCaller
+# from .browser import MHAssetBrowserModel, AssetBrowserFrame
 from .styles import window_style, button_style
 import omni.ui as ui
 import omni.kit.ui
 import omni
 import carb
+from . import mhusd
 
 WINDOW_TITLE = "Human Generator"
 MENU_PATH = f"Window/{WINDOW_TITLE}"
@@ -35,21 +34,19 @@ class MHWindow(ui.Window):
 
         super().__init__(title)
 
-        # Holds the state of the realtime toggle
-        self.toggle_model = ui.SimpleBoolModel()
-        # Holds the state of the parameter list
-        self.param_model = ParamPanelModel(self.toggle_model)
-        # Keep track of the human
-        self._human = Human()
+        # # Holds the state of the realtime toggle
+        # self.toggle_model = ui.SimpleBoolModel()
+        # # Holds the state of the parameter list
+        self.param_model = ParamPanelModel()
 
-        # A model to hold browser data
-        self.browser_model = MHAssetBrowserModel(
-            self._human,
-            filter_file_suffixes=["mhpxy", "mhskel", "mhclo"],
-            timeout=carb.settings.get_settings().get(
-                "/exts/siborg.create.human.browser.asset/data/timeout"
-            ),
-        )
+        # # A model to hold browser data
+        # self.browser_model = MHAssetBrowserModel(
+        #     self._human,
+        #     filter_file_suffixes=["mhpxy", "mhskel", "mhclo"],
+        #     timeout=carb.settings.get_settings().get(
+        #         "/exts/siborg.create.human.browser.asset/data/timeout"
+        #     ),
+        # )
 
 
         # Subscribe to selection events on the message bus
@@ -69,39 +66,38 @@ class MHWindow(ui.Window):
 
                 self.property_panel = ui.HStack(visible=False)
                 with self.property_panel:
-                    with ui.ZStack(width=0):
-                        # Draggable splitter
-                        with ui.Placer(offset_x=self.frame.computed_content_width/1.8, draggable=True, drag_axis=ui.Axis.X):
-                            ui.Rectangle(width=spacer_width, name="splitter")
-                        with ui.HStack():
-                            # Left-most panel is a browser for MakeHuman assets. It includes
-                            # a reference to the list of applied proxies so that an update
-                            # can be triggered when new assets are added
-                            self.browser = AssetBrowserFrame(self.browser_model)
-                            ui.Spacer(width=spacer_width)
+                    # with ui.ZStack(width=0):
+                    #     # Draggable splitter
+                    #     with ui.Placer(offset_x=self.frame.computed_content_width/1.8, draggable=True, drag_axis=ui.Axis.X):
+                    #         ui.Rectangle(width=spacer_width, name="splitter")
+                    #     with ui.HStack():
+                    #         # Left-most panel is a browser for MakeHuman assets. It includes
+                    #         # a reference to the list of applied proxies so that an update
+                    #         # can be triggered when new assets are added
+                    #         self.browser = AssetBrowserFrame(self.browser_model)
+                    #         ui.Spacer(width=spacer_width)
                     with ui.HStack():
                         with ui.VStack():
-                            self.param_panel = ParamPanel(self.param_model,self.update_human)
-                            with ui.HStack(height=0):
+                            self.param_panel = ParamPanel(self.param_model)
+                            # with ui.HStack(height=0):
                                 # Toggle whether changes should propagate instantly
-                                ui.ToolButton(text = "Update Instantly", model = self.toggle_model)
+                                # ui.ToolButton(text = "Update Instantly", model = self.toggle_model)
                 with ui.VStack(width = 100, style=button_style):
                     # Creates a new human in scene and resets modifiers and assets
                     ui.Button(
                         "New Human",
-                        clicked_fn=self.new_human,
-                    )
-                    # Updates current human in omniverse scene
-                    self.update_button = ui.Button(
-                        "Update Human",
-                        clicked_fn=self.update_human,
-                        enabled=False,
+                        clicked_fn=mhusd.add_to_scene,
                     )
                     # Resets modifiers and assets on selected human
                     self.reset_button = ui.Button(
                         "Reset Human",
                         clicked_fn=self.reset_human,
                         enabled=False,
+                    )
+                    # Build a human from scratch
+                    ui.Button(
+                        "Build Human",
+                        clicked_fn=mhusd.make_human,
                     )
 
 
@@ -128,8 +124,7 @@ class MHWindow(ui.Window):
             # Show the no selection notification
             self.no_selection_notification.visible = True
 
-            # Deactivate the update and reset buttons
-            self.update_button.enabled = False
+            # Deactivate the reset button
             self.reset_button.enabled = False
 
         else:
@@ -140,52 +135,31 @@ class MHWindow(ui.Window):
             # Hide the no selection notification
             self.no_selection_notification.visible = False
 
-            # Activate the update and reset buttons
-            self.update_button.enabled = True
+            # Activate the reset button
             self.reset_button.enabled = True
 
             # Get the prim from the path in the event payload
             prim = stage.GetPrimAtPath(prim_path)
 
-            # Update the human in MHCaller
-            self._human.set_prim(prim)
+            # # Update the human in MHCaller
+            # self._human.set_prim(prim)
 
             # Update the list of applied modifiers
             self.param_panel.load_values(prim)
 
-    def new_human(self):
-        """Creates a new human in the scene and selects it"""
-        
-        # Reset the human class
-        self._human.reset()
 
-        # Create a new human
-        self._human.prim = self._human.add_to_scene()
-
-        # Get selection.
-        selection = omni.usd.get_context().get_selection()
-        # Select the new human.
-        selection.set_selected_prim_paths([self._human.prim_path], True)
-
-    def update_human(self):
-        """Updates the current human in the scene"""
-        # Collect changed values from the parameter panel
-        self.param_panel.update_models()
-
-
-        # Update the human in the scene
-        self._human.update_in_scene(self._human.prim_path)
 
     def reset_human(self):
-        """Resets the current human in the scene"""
-        # Reset the human
-        self._human.reset()
+        # """Resets the current human in the scene"""
+        # # Reset the human
+        # self._human.reset()
 
-        # Delete the proxy prims
-        self._human.delete_proxies()
+        # # Delete the proxy prims
+        # self._human.delete_proxies()
 
-        # Update the human in the scene and reset parameter widgets
-        self.update_human()
+        # # Update the human in the scene and reset parameter widgets
+        # self.update_human()
+        pass
 
     def destroy(self):
         """Called when the window is destroyed. Unsuscribes from human selection events"""
